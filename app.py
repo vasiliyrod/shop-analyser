7beebb513eb1b0c8411fb1d086569abe8cccb049
Vladislav <vladislav.korshun.2005@gmail.com>
2024-06-13 16:30:58 +0300

    Загрузка пользовательского файла
    1. Добавлена возможность загрузки .xls или .csv файла с данными
    2. Обновлена цветовая палитра сайта
    3. Добавлен файл README

--- app.py
+++ app.py
@@ -1,18 +1,10 @@
 import dash
-from dash import dcc, html
+from dash import dcc, html, Input, Output, State
 import plotly.express as px
 import pandas as pd
-
-# Загрузка датасета
-df = pd.read_csv('data/grocery_store_data.csv')
-
-# Создание возрастных групп
-age_bins = [0, 18, 25, 35, 60, 100]
-age_labels = ['<18', '18-25', '26-35', '36-60', '60+']
-df['Возрастная группа'] = pd.cut(df['Возраст'], bins=age_bins, labels=age_labels, right=False)
-
-# Упорядочиваем столбец 'Возрастная группа' по возрастанию
-df.sort_values(by='Возрастная группа', inplace=True)
+import base64
+import io
+import datetime
 
 # Создание приложения Dash
 app = dash.Dash(__name__)
@@ -21,46 +13,108 @@ app = dash.Dash(__name__)
 app.layout = html.Div(className='container', children=[
     html.H1("Анализ данных продуктового магазина"),
 
+    # Загрузка файла
+    html.Div(className='upload-data', children=[
+        dcc.Upload(
+            id='upload-data',
+            children=html.Div(['Перетащите или ', html.A('выберите файл')]),
+            style={
+                'width': '100%',
+                'height': '60px',
+                'lineHeight': '60px',
+                'borderWidth': '1px',
+                'borderStyle': 'dashed',
+                'borderRadius': '5px',
+                'textAlign': 'center',
+                'margin': '10px',
+                'background-color': '#e9ecef'
+            },
+            multiple=False
+        ),
+        html.Div(id='output-data-upload')
+    ]),
+
     # Первая диаграмма: "мужчина-женщина", круговая
     html.Div(className='graph', children=[
         html.H2("Мужчина-Женщина"),
-        dcc.Graph(
-            id='gender-dist',
-            figure=px.pie(df, names='Пол', title='Мужчина-Женщина')
-        )
+        dcc.Graph(id='gender-dist')
     ]),
 
     # Вторая диаграмма: "возраст", столбчатая
     html.Div(className='graph', children=[
         html.H2("Возраст"),
-        dcc.Graph(
-            id='age-dist',
-            figure=px.histogram(df, x='Возрастная группа', title='Возраст')
-        )
+        dcc.Graph(id='age-dist')
     ]),
 
     # Третья диаграмма: "дни недели", столбчатая
     html.Div(className='graph', children=[
         html.H2("Дни недели"),
-        dcc.Graph(
-            id='day-dist',
-            figure=px.histogram(df, x='День недели', title='Дни недели', category_orders={'День недели': ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье']})
-        )
+        dcc.Graph(id='day-dist')
     ]),
 
     # Четвёртая диаграмма: "вывод", столбчатая
     html.Div(className='graph', children=[
         html.H2("Вывод"),
-        dcc.Graph(
-            id='amount-by-age-gender',
-            figure=px.histogram(df, x='Возрастная группа', y='Потраченная сумма', color='Пол', barmode='group', title='Потраченная сумма по возрасту и полу')
-        )
+        dcc.Graph(id='amount-by-age-gender')
     ]),
 
     html.Div(className='footer', children=[
-        "Разработано с использованием Dash и Plotly"
+        "Copyright © Змеееды, 2024. Никакие права не защищены."
     ])
 ])
+
+def parse_contents(contents, filename, date):
+    content_type, content_string = contents.split(',')
+    decoded = base64.b64decode(content_string)
+    try:
+        if 'csv' in filename:
+            df = pd.read_csv(io.StringIO(decoded.decode('utf-8')))
+        elif 'xls' in filename:
+            df = pd.read_excel(io.BytesIO(decoded))
+        else:
+            return html.Div(['Недопустимый формат файла. Пожалуйста, загрузите файл в формате CSV или Excel.'])
+
+        return df
+    except Exception as e:
+        print(e)
+        return html.Div(['Произошла ошибка при обработке файла.'])
+
+@app.callback(
+    Output('output-data-upload', 'children'),
+    Output('gender-dist', 'figure'),
+    Output('age-dist', 'figure'),
+    Output('day-dist', 'figure'),
+    Output('amount-by-age-gender', 'figure'),
+    Input('upload-data', 'contents'),
+    State('upload-data', 'filename'),
+    State('upload-data', 'last_modified')
+)
+def update_output(contents, filename, date):
+    if contents is not None:
+        df = parse_contents(contents, filename, date)
+
+        age_bins = [0, 18, 25, 35, 60, 100]
+        age_labels = ['<18', '18-25', '26-35', '36-60', '60+']
+        df['Возрастная группа'] = pd.cut(df['Возраст'], bins=age_bins, labels=age_labels, right=False)
+        df.sort_values(by='Возрастная группа', inplace=True)
+
+        color_map = {'Мужской': 'blue', 'Женский': 'red'}
+        age_color = ['yellow']
+
+        gender_fig = px.pie(df, names='Пол', title='Мужчина-Женщина', color='Пол', color_discrete_map=color_map)
+        age_fig = px.histogram(df, x='Возрастная группа', title='Возраст', color_discrete_sequence=age_color)
+        day_fig = px.histogram(df, x='День недели', title='Дни недели', category_orders={'День недели': ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье']})
+        amount_fig = px.histogram(df, x='Возрастная группа', y='Потраченная сумма', color='Пол', barmode='group', title='Потраченная сумма по возрасту и полу', color_discrete_map=color_map)
+
+        return (html.Div([
+            html.H5(filename),
+            html.H6(datetime.datetime.fromtimestamp(date))]),
+            gender_fig,
+            age_fig,
+            day_fig,
+            amount_fig)
+
+    return html.Div(['Пожалуйста, загрузите файл.']), {}, {}, {}, {}
 
 # Запуск приложения
 if __name__ == '__main__':
